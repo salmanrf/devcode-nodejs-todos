436206474ea581858762b7d3b27812ee7e826ec1
salmanrf <salmanrf2@gmail.com>
2023-02-17 18:03:36 +0700

    fix: use env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,28 +6,15 @@ import { ActivitiesModule } from './activities/activities.module';
 import { TodosModule } from './todos/todos.module';
 import { DB_CONFIG } from './configs/db.config';
 
-console.log('DB_CONFIG', DB_CONFIG);
-
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: DB_CONFIG.MYSQL_HOST,
-    //   username: DB_CONFIG.MYSQL_USER,
-    //   password: DB_CONFIG.MYSQL_PASSWORD,
-    //   database: DB_CONFIG.MYSQL_DBNAME,
-    //   port: +DB_CONFIG.MYSQL_PORT,
-    //   entities: ['dist/**/*.entity{.ts,.js}'],
-    //   logging: true,
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      username: 'salmanrf',
-      password: 'levitation123',
-      database: 'todos',
-      port: 3307,
+      host: DB_CONFIG.MYSQL_HOST,
+      username: DB_CONFIG.MYSQL_USER,
+      password: DB_CONFIG.MYSQL_PASSWORD,
+      database: DB_CONFIG.MYSQL_DBNAME,
+      port: +DB_CONFIG.MYSQL_PORT,
       entities: ['dist/**/*.entity{.ts,.js}'],
       logging: true,
       synchronize: true,
